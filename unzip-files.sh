ef7037a780da158f70e792bb50d2e57d926d416f
PaulW <paulwamp@gmail.com>
2011-05-17 10:47:46 +0100

    Fixed a typo
    Change-Id: I632ce0bcd42f36448b07c4b89bc82380835ad46a

--- unzip-files.sh
+++ unzip-files.sh
@@ -47,7 +47,7 @@ unzip -j -o ../../../${DEVICE}_update.zip system/lib/libmmjpeg.so -d ../../../ve
 unzip -j -o ../../../${DEVICE}_update.zip system/lib/liboemcamera.so -d ../../../vendor/$MANUFACTURER/$DEVICE/proprietary
 unzip -j -o ../../../${DEVICE}_update.zip system/lib/libposteffect.so -d ../../../vendor/$MANUFACTURER/$DEVICE/proprietary
 
-(cat << EOF) | sed s/__DEVICE__/$DEVICE/g | sed s/__MANUFACTURER__/$MANUFACTURER/g > ../../../vendor/$MANUFACTURER/$DEVICE/device-vendor-blobs.mk
+(cat << EOF) | sed s/__DEVICE__/$DEVICE/g | sed s/__MANUFACTURER__/$MANUFACTURER/g > ../../../vendor/$MANUFACTURER/$DEVICE/$DEVICE-vendor-blobs.mk
 # Copyright (C) 2010 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
